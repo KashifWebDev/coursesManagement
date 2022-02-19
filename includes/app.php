6cbd4d6bfd5204d929c3795b3e3c15b7a389fa7a
Kaskhif Ali <kmalik748@gmail.com>
2022-02-19 11:10:49 +0500

    1

--- includes/app.php
+++ includes/app.php
@@ -1,13 +1,13 @@
 <?php
-const ROOT_DIR = "http://localhost:8080/courses/";
+const ROOT_DIR = "https://teachmehow.me/";
 
-$con = mysqli_connect("localhost","root","","project_18j");
-//$con = mysqli_connect("localhost","j18jocnn_18joris","CPvvGapgy)Oy","j18jocnn_18jor");
-
-// Check connections
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-
-$GLOBALS["con"] = $con;
+//$con = mysqli_connect("localhost","root","","project_18j");
+////$con = mysqli_connect("localhost","j18jocnn_18joris","CPvvGapgy)Oy","j18jocnn_18jor");
+//
+//// Check connections
+//if (mysqli_connect_errno()) {
+//    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//    exit();
+//}
+//
+//$GLOBALS["con"] = $con;
